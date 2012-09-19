42d40c851e242e13f700d65d8891a6a5fc7ee644
Matt Weber <matt@mattweber.org>
2012-09-19 08:33:30 -0700

    Add delete_data option to JSON adapter
    Added the delete_data option to the JSON input adapter that
    will remove the data field from the incomming packet before
    a merge.  This option is disabled by default, and when enabled
    will restore the data field when there is a merge error.

--- ui/pypesvds/plugins/simplejsonadapter/simplejsonadapter.py
+++ ui/pypesvds/plugins/simplejsonadapter/simplejsonadapter.py
@@ -11,10 +11,25 @@ class SimpleJSON(Component):
 
     def __init__(self):
         Component.__init__(self)
+        self.set_parameter('delete_data', 'False', ['True', 'False'])
         log.info('Component Initialized: %s' % self.__class__.__name__)
 
     def run(self):
         while True:
+            # get parameters outside doc loop for better performace
+            try:
+                delete_data = True if self.get_parameter('delete_data') == 'True' else False
+            except Exception as e:
+                log.error('Component Failed: %s' % self.__class__.__name__)
+                log.error('Reason: %s' % str(e))
+
+                # optionally send all docs without processing
+                for d in self.receive_all('in'):
+                    self.send('out', d)
+
+                self.yield_ctrl()
+                continue # so next time we are called we continue at the top               
+
             for doc in self.receive_all('in'):
                 try:
                     data = doc.get('data')
@@ -27,11 +42,17 @@ class SimpleJSON(Component):
                             log.warn('Ignorning doc: %s' % doc.get('id'))
                             continue
                         else:
+                            if delete_data:
+                                doc.delete('data')
+
                             try:
                                 doc.merge(Packet(json.loads(data)), metas=True)
                             except:
                                 log.error('Unable to convert data')
                                 log.error(traceback.print_exc())
+                                # restore data field if if was deleted before error
+                                if delete_data:
+                                    doc.set('data', data)
 
                 except Exception as e:
                     log.error('Component Failed: %s' % self.__class__.__name__)
