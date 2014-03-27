640f345678fda58f2d29021dadd939ffa040e3d1
Matteo Abis <gmatteo..abis@gmail.com>
2014-03-27 10:12:39 +0100

    added zmq push

--- core/pypes/plugins/zmq.py
+++ core/pypes/plugins/zmq.py
@@ -4,7 +4,8 @@ import zmq
 
 from pypes.component import Component
 
-class ZmqReplier(Component):
+
+class ZmqReply(Component):
     """Note: The call blocks, so a request must be send through zmq,
     otherwise the pipeline hangs!"""
 
@@ -24,5 +25,28 @@ class ZmqReplier(Component):
             for packet in self.receive_all('in'):
                 # if requested through the socket, I will send the data
                 message = socket.recv_string()
+                socket.send_pyobj(packet)
+            self.yield_ctrl()
+
+
+class ZmqPush(Component):
+    """Note: The call blocks, so a request must be send through zmq,
+    otherwise the pipeline hangs!"""
+
+    __metatype__ = 'PUBLISHER'
+
+    def __init__(self, port=40000):
+        Component.__init__(self)
+        self.set_parameter("port", port)
+        self.set_parameter("name", None)
+
+    def run(self):
+        port = self.get_parameter("port")
+        context = zmq.Context()
+        socket = context.socket(zmq.PUSH)
+        socket.bind("tcp://*:{0}".format(port))
+        while True:
+            for packet in self.receive_all('in'):
+                # if requested through the socket, I will send the data
                 socket.send_pyobj(packet)
             self.yield_ctrl()
